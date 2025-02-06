230e83e980db4535c81b7350f4ba74516315be90
David Dada <daveproxy80@gmail.com>
2025-02-06 07:59:57 +0100

    init script

--- src/initializers/KOVAInit.sol
+++ src/initializers/KOVAInit.sol
@@ -6,13 +6,19 @@ import {IDiamondLoupe} from "src/interfaces/IDiamondLoupe.sol";
 import {IDiamondCut} from "src/interfaces/IDiamondCut.sol";
 import {IERC7662} from "src/interfaces/IERC7662.sol";
 import {IERC165} from "@openzeppelin/contracts/interfaces/IERC165.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {IERC4906} from "@openzeppelin/contracts/interfaces/IERC4906.sol";
 import {IAccessControl} from "@openzeppelin/contracts/access/IAccessControl.sol";
 import {IERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
+import {IERC721Metadata} from "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+import {AgentNFTFacet} from "src/facets/AgentNFTFacet.sol";
+import {LibERC721} from "src/libraries/LibERC721.sol";
 
 /// @notice Initialize.
 /// @author KOVA (https://github.com/KOVA-A/solidity-contract)
 /// @author Modified from Nick Mudge (https://github.com/mudgen/diamond-3)
-contract ERC165Init {
+contract KOVAInit {
+    AgentNFTFacet agentNFTFacet;
     function init() external {
         // adding ERC165 data
         LibDiamond.DiamondStorage storage ds = LibDiamond.diamondStorage();
@@ -21,7 +27,12 @@ contract ERC165Init {
         ds.supportedInterfaces[type(IDiamondLoupe).interfaceId] = true;
         ds.supportedInterfaces[type(IAccessControl).interfaceId] = true;
         ds.supportedInterfaces[type(IERC7662).interfaceId] = true;
+        ds.supportedInterfaces[type(IERC721).interfaceId] = true;
         ds.supportedInterfaces[type(IERC721Enumerable).interfaceId] = true;
+        ds.supportedInterfaces[type(IERC721Metadata).interfaceId] = true;
+        ds.supportedInterfaces[type(IERC4906).interfaceId] = true;
+
+        agentNFTFacet.initialize();
 
         // EIP-2535 specifies that the `diamondCut` function takes two optional
         // arguments: address _init and bytes calldata _calldata
