11cafeea966723e6124356db048cbd9087f8c18e
David Dada <daveproxy80@gmail.com>
2025-02-07 13:46:53 +0100

    move room logic from library to main contract

--- src/facets/AgentRoomFacet.sol
+++ src/facets/AgentRoomFacet.sol
@@ -1,41 +1,108 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.0;
 
+import {AgentNFT} from "src/agentNFT/AgentNFT.sol";
 import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
-
 import {LibAgentRoom} from "src/libraries/LibAgentRoom.sol";
-
+import {EnumerableMap} from "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
 import "src/libraries/constants/Events.sol";
 import "src/libraries/constants/Errors.sol";
 import "src/libraries/constants/Types.sol";
 
 contract AgentRoomFacet is IERC721Receiver {
-    function createRoom(uint8 roomType, uint256 agentID) external returns (uint256 roomId) {
-        return LibAgentRoom.createRoom(roomType, agentID);
+    using EnumerableMap for EnumerableMap.UintToUintMap;
+
+    uint256 private constant MAX_AGENTS = 2;
+
+    AgentNFT private constant AGENTNFT =
+        AgentNFT(0xEF78E7D23A02a404D348a0f37ac0fF4D10991D1a);
+
+    function createRoom(
+        uint8 roomType,
+        uint256 agentID
+    ) external returns (uint256 roomId) {
+        if (AGENTNFT.ownerOf(agentID) != msg.sender) {
+            revert AgentRoom__OnlyOwnerCanCreateRoom();
+        }
+
+        AgentType agentType;
+        (agentType, , , ) = AGENTNFT.getAgentExtraData(agentID);
+        if (agentType == AgentType.Investor) {
+            revert AgentRoom__OnlyTraderCanCreateRoom();
+        }
+
+        bytes32 roomIdHash = keccak256(abi.encode(agentID, roomType));
+
+        roomId = uint256(roomIdHash);
+        LibAgentRoom._getAgentRoomStorage().roomAgents[roomId].push(agentID);
+        LibAgentRoom._getAgentRoomStorage().roomAgents2[roomId].set(0, agentID);
+        emit RoomCreated(roomId, roomType, agentID);
     }
 
     function joinRoom(uint256 roomId, uint8 roomType, uint256 agentID) public {
-        LibAgentRoom.joinRoom(roomId, roomType, agentID);
+        if (AGENTNFT.ownerOf(agentID) != msg.sender) {
+            revert AgentRoom__OnlyOwnerCanCreateRoom();
+        }
+
+        AgentType agentType;
+        (agentType, , , ) = AGENTNFT.getAgentExtraData(agentID);
+        if (agentType == AgentType.Trader) {
+            revert AgentRoom__OnlyInvestorsCanJoinRoom();
+        }
+
+        LibAgentRoom.AgentRoomStorage storage $ = LibAgentRoom._getAgentRoomStorage();
+        uint256[] memory agentIDs = $.roomAgents[roomId];
+        $.roomAgents[roomId].push(agentID);
+        uint256 agentRoomPosition = $.roomAgents2[roomId].length();
+        $.roomAgents2[roomId].set(agentRoomPosition, agentID);
+
+        if (agentIDs.length > MAX_AGENTS) {
+            emit RoomFull(roomId, roomType, agentIDs);
+            revert AgentRoom__MaxAgentsExceeded();
+        }
     }
 
     function leaveRoom(uint256 roomId, uint8 roomType, uint256 agentID) public {
-        LibAgentRoom.leaveRoom(roomId, roomType, agentID);
+        emit RoomLeft(roomId, roomType, agentID);
     }
 
     function getRoomParticipant(uint256 roomId) public view returns (uint256) {
-        return LibAgentRoom.getRoomParticipant(roomId);
+        return LibAgentRoom._getAgentRoomStorage().roomAgents2[roomId].get(roomId);
     }
 
-    function getRoomParticipants(uint256 roomId) public view returns (uint256[] memory) {
-        return LibAgentRoom.getRoomParticipants(roomId);
+    function getRoomParticipants(
+        uint256 roomId
+    ) public view returns (uint256[] memory) {
+        return LibAgentRoom._getAgentRoomStorage().roomAgents[roomId];
     }
 
-    function getAllRoomParticipants(uint256 roomId) public view returns (uint256[] memory agentIDs) {
-        return LibAgentRoom.getAllRoomParticipants(roomId);
+    function getAllRoomParticipants(
+        uint256 roomId
+    ) public view returns (uint256[] memory agentIDs) {
+        uint256 agentRoomLength = LibAgentRoom._getAgentRoomStorage()
+            .roomAgents2[roomId]
+            .length();
+        for (uint256 i; i < agentRoomLength; ) {
+            agentIDs[i] = LibAgentRoom._getAgentRoomStorage().roomAgents2[roomId].get(i);
+            unchecked {
+                ++i;
+            }
+        }
     }
 
-    function viewAgentRoomStatus(uint256 agentID) public view returns (string memory) {
-        return LibAgentRoom.viewAgentRoomStatus(agentID);
+    function viewAgentRoomStatus(
+        uint256 roomId
+    ) public view returns (string memory) {
+        uint256 agentRoomLength = LibAgentRoom._getAgentRoomStorage()
+            .roomAgents2[roomId]
+            .length();
+        if (agentRoomLength == MAX_AGENTS) {
+            return "In progress";
+        } else if (agentRoomLength == 1) {
+            return "Open";
+        } else {
+            return "Closed";
+        }
     }
 
     /**
@@ -43,7 +110,12 @@ contract AgentRoomFacet is IERC721Receiver {
      *
      * Always returns `IERC721Receiver.onERC721Received.selector`.
      */
-    function onERC721Received(address, address, uint256, bytes memory) public virtual returns (bytes4) {
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes memory
+    ) public virtual returns (bytes4) {
         return this.onERC721Received.selector;
     }
 }
--- src/libraries/LibAgentRoom.sol
+++ src/libraries/LibAgentRoom.sol
@@ -1,15 +1,12 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.0;
 
-import {AgentNFT} from "src/agentNFT/AgentNFT.sol";
 import {EnumerableMap} from "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
 import "src/libraries/constants/Types.sol";
 import "src/libraries/constants/Errors.sol";
 import "src/libraries/constants/Events.sol";
 
 library LibAgentRoom {
-    using EnumerableMap for EnumerableMap.UintToUintMap;
-
     struct AgentRoomStorage {
         EnumerableMap.UintToUintMap agentRooms;
         mapping(uint256 => EnumerableMap.UintToUintMap) roomAgents2;
@@ -23,88 +20,84 @@ library LibAgentRoom {
     bytes32 private constant AGENTROOM_STORAGE_LOCATION =
         0x0684d24082dd6910851205f7b7dcea9a01cec5d71b932f67cc926c6c18323c00;
 
-    AgentNFT private constant AGENTNFT = AgentNFT(0xEF78E7D23A02a404D348a0f37ac0fF4D10991D1a);
-
     function _getAgentRoomStorage() internal pure returns (AgentRoomStorage storage $) {
         assembly {
             $.slot := AGENTROOM_STORAGE_LOCATION
         }
     }
 
-    uint256 private constant MAX_AGENTS = 2;
+    // function createRoom(uint8 roomType, uint256 agentID) external returns (uint256 roomId) {
+    //     if (AGENTNFT.ownerOf(agentID) != msg.sender) {
+    //         revert AgentRoom__OnlyOwnerCanCreateRoom();
+    //     }
 
-    function createRoom(uint8 roomType, uint256 agentID) external returns (uint256 roomId) {
-        if (AGENTNFT.ownerOf(agentID) != msg.sender) {
-            revert AgentRoom__OnlyOwnerCanCreateRoom();
-        }
+    //     AgentType agentType;
+    //     (agentType,,,) = AGENTNFT.getAgentExtraData(agentID);
+    //     if (agentType == AgentType.Investor) {
+    //         revert AgentRoom__OnlyTraderCanCreateRoom();
+    //     }
 
-        AgentType agentType;
-        (agentType,,,) = AGENTNFT.getAgentExtraData(agentID);
-        if (agentType == AgentType.Investor) {
-            revert AgentRoom__OnlyTraderCanCreateRoom();
-        }
+    //     bytes32 roomIdHash = keccak256(abi.encode(agentID, roomType));
 
-        bytes32 roomIdHash = keccak256(abi.encode(agentID, roomType));
+    //     roomId = uint256(roomIdHash);
+    //     _getAgentRoomStorage().roomAgents[roomId].push(agentID);
+    //     _getAgentRoomStorage().roomAgents2[roomId].set(0, agentID);
+    //     emit RoomCreated(roomId, roomType, agentID);
+    // }
 
-        roomId = uint256(roomIdHash);
-        _getAgentRoomStorage().roomAgents[roomId].push(agentID);
-        _getAgentRoomStorage().roomAgents2[roomId].set(0, agentID);
-        emit RoomCreated(roomId, roomType, agentID);
-    }
+    // function joinRoom(uint256 roomId, uint8 roomType, uint256 agentID) public {
+    //     if (AGENTNFT.ownerOf(agentID) != msg.sender) {
+    //         revert AgentRoom__OnlyOwnerCanCreateRoom();
+    //     }
 
-    function joinRoom(uint256 roomId, uint8 roomType, uint256 agentID) public {
-        if (AGENTNFT.ownerOf(agentID) != msg.sender) {
-            revert AgentRoom__OnlyOwnerCanCreateRoom();
-        }
+    //     AgentType agentType;
+    //     (agentType,,,) = AGENTNFT.getAgentExtraData(agentID);
+    //     if (agentType == AgentType.Trader) {
+    //         revert AgentRoom__OnlyInvestorsCanJoinRoom();
+    //     }
 
-        AgentType agentType;
-        (agentType,,,) = AGENTNFT.getAgentExtraData(agentID);
-        if (agentType == AgentType.Trader) {
-            revert AgentRoom__OnlyInvestorsCanJoinRoom();
-        }
+    //     AgentRoomStorage storage $ = _getAgentRoomStorage();
+    //     uint256[] memory agentIDs = $.roomAgents[roomId];
+    //     $.roomAgents[roomId].push(agentID);
+    //     uint256 agentRoomPosition = $.roomAgents2[roomId].length();
+    //     $.roomAgents2[roomId].set(agentRoomPosition, agentID);
 
-        AgentRoomStorage storage $ = _getAgentRoomStorage();
-        uint256[] memory agentIDs = $.roomAgents[roomId];
-        $.roomAgents[roomId].push(agentID);
-        uint256 agentRoomPosition = $.roomAgents2[roomId].length();
-        $.roomAgents2[roomId].set(agentRoomPosition, agentID);
+    //     if (agentIDs.length > MAX_AGENTS) {
+    //         emit RoomFull(roomId, roomType, agentIDs);
+    //         revert AgentRoom__MaxAgentsExceeded();
+    //     }
+    // }
 
-        if (agentIDs.length > MAX_AGENTS) {
-            emit RoomFull(roomId, roomType, agentIDs);
-            revert AgentRoom__MaxAgentsExceeded();
-        }
-    }
+    // function leaveRoom(uint256 roomId, uint8 roomType, uint256 agentID) public {
+    //     emit RoomLeft(roomId, roomType, agentID);
+    // }
 
-    function leaveRoom(uint256 roomId, uint8 roomType, uint256 agentID) public {
-        emit RoomLeft(roomId, roomType, agentID);
-    }
+    // function getRoomParticipant(uint256 roomId) public view returns (uint256) {
+    //     return _getAgentRoomStorage().roomAgents2[roomId].get(roomId);
+    // }
 
-    function getRoomParticipant(uint256 roomId) public view returns (uint256) {
-        return _getAgentRoomStorage().roomAgents2[roomId].get(roomId);
-    }
+    // function getRoomParticipants(uint256 roomId) public view returns (uint256[] memory) {
+    //     return _getAgentRoomStorage().roomAgents[roomId];
+    // }
 
-    function getRoomParticipants(uint256 roomId) public view returns (uint256[] memory) {
-        return _getAgentRoomStorage().roomAgents[roomId];
-    }
+    // function getAllRoomParticipants(uint256 roomId) public view returns (uint256[] memory agentIDs) {
+    //     uint256 agentRoomLength = _getAgentRoomStorage().roomAgents2[roomId].length();
+    //     for (uint256 i; i < agentRoomLength;) {
+    //         agentIDs[i] = _getAgentRoomStorage().roomAgents2[roomId].get(i);
+    //         unchecked {
+    //             ++i;
+    //         }
+    //     }
+    // }
 
-    function getAllRoomParticipants(uint256 roomId) public view returns (uint256[] memory agentIDs) {
-        uint256 agentRoomLength = _getAgentRoomStorage().roomAgents2[roomId].length();
-        for (uint256 i; i < agentRoomLength;) {
-            agentIDs[i] = _getAgentRoomStorage().roomAgents2[roomId].get(i);
-            unchecked {
-                ++i;
-            }
-        }
-    }
-
-    function viewAgentRoomStatus(uint256 roomId) public view returns (string memory) {
-        uint256 agentRoomLength = _getAgentRoomStorage().roomAgents2[roomId].length();
-        if (agentRoomLength == MAX_AGENTS) {
-            return "In progress";
-        } else if (agentRoomLength == 1) {
-            return "Open";
-        } else {
-            return "Closed";
-        }
-    }
+    // function viewAgentRoomStatus(uint256 roomId) public view returns (string memory) {
+    //     uint256 agentRoomLength = _getAgentRoomStorage().roomAgents2[roomId].length();
+    //     if (agentRoomLength == MAX_AGENTS) {
+    //         return "In progress";
+    //     } else if (agentRoomLength == 1) {
+    //         return "Open";
+    //     } else {
+    //         return "Closed";
+    //     }
+    // }
 }
