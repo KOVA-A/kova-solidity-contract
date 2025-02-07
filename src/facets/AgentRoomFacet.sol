a2f7c72606c8d4e59314fe8e70ed71e7599e2126
David Dada <daveproxy80@gmail.com>
2025-02-07 10:24:08 +0100

    add more view functions to the room

--- src/facets/AgentRoomFacet.sol
+++ src/facets/AgentRoomFacet.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.0;
 
+import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+
 import {LibERC721} from "src/libraries/LibERC721.sol";
 import {LibAgentRoom} from "src/libraries/LibAgentRoom.sol";
 import {LibAgentNFT} from "src/libraries/LibAgentNFT.sol";
-import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+
 import "src/libraries/constants/Events.sol";
 import "src/libraries/constants/Errors.sol";
 import "src/libraries/constants/Types.sol";
@@ -27,6 +29,22 @@ contract AgentRoomFacet is IERC721Receiver {
         uint256 agentID
     ) public {
         LibAgentRoom.leaveRoom(roomId, roomType, agentID);
+    }
+
+    function getRoomParticipant(uint256 roomId) public view returns (uint256) {
+        return LibAgentRoom.getRoomParticipant(roomId);
+    }
+
+    function getRoomParticipants(uint256 roomId) public view returns (uint256[] memory) {
+        return LibAgentRoom.getRoomParticipants(roomId);
+    }
+
+    function getAllRoomParticipants(uint256 roomId) public view returns (uint256[] memory agentIDs) {
+        return LibAgentRoom.getAllRoomParticipants(roomId);
+    }
+
+    function viewAgentRoomStatus(uint256 agentID) public view returns (string memory) {
+        return LibAgentRoom.viewAgentRoomStatus(agentID);
     }
 
     /**
